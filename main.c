91fe570f37136949eb9b740c14b3c131bd9a0a62
yuetong <tom880112@gmail.com>
2018-05-18 10:30:44 +0800

    移除不必要的檔案

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[]){
+	int a,b,sum;
+	printf("請輸入第一個數字\n");
+	scanf("%d",&a);
+	printf("請輸入第二個數字\n");
+	scanf("%d",&b);
+	
+	sum=a+b;
+	printf("總和為%d\n",sum);
+	return 0;
 }
-
